a3b536962a2c62b7652967e86a7822c28427a051
Dawalhasa <dawalhasa@gmail.com>
2025-06-02 15:14:41 +0000

    React installation for linux Ubuntu successfully complete

--- OS/linux/linux_os.sh
+++ OS/linux/linux_os.sh
@@ -2,21 +2,27 @@
 
 OS=$(uname -s)
 Distro=$(uname -n)
+echo 'Provite your vite-Project Name:'
+
+
+read name
 
 # install react on linux distro
 if [[ "$OS" == "Linux" && "$Distro" == "Ubuntu" ]]; then
   sudo apt update
   sudo apt install -y nodejs npm
-  npm install -g create-react-app
+  npm create vite@latest $name
   echo "React installed via apt on Ubuntu."
 elif [[ "$OS" == "Linux" && "$Distro" == "Archi" ]]; then
   sudo pacman update
   sudo pacman install -y nodejs npm
-  npm install -g create-react-app
+  npm create vite@latest $name
   echo "React installed via apt on Archi Linux."
-else [[ "$OS" == "Linux" && "$Distro" == "CentOS" ]]; then
-  sudo yum update
-  sudo yum install -y nodejs npm
-  npm install -g create-react-app
-  echo "React installed via apt on CentOS."
+elif [[ "$OS" == "Linux" && "$Distro" == "CentOS" ]]; then
+   sudo yum update
+   sudo yum install -y nodejs npm
+   npm install -g create-react-app
+   echo "React installed via apt on CentOS."
+else
+	echo "Your OS don't support react installation"
 fi
