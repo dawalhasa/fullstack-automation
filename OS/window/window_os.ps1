20b989389ab0efc497d68509d0bcf3b3cddcfb28
Dawalhasa <dawalhasa@gmail.com>
2025-06-04 10:52:56 +0530

    Build fully functioning powershell script for frontend application

--- OS/window/window_os.ps1
+++ OS/window/window_os.ps1
@@ -1,7 +1,21 @@
-function Get-InstallReact{
+<#
+.SYNOPSIS
+FrontEnd
+.DESCRIPTION
+This is fullstack FrontEnd Application automation tool for Web-Developer
+.EXAMPLE
+Get-Frontend -Projectname myreact-app
+.EXAMPLE
+Get-Frontend 
+cmdlet Get-Frontend at command pipeline position 1
+Supply values for the following parameters:
+Projectname:
+#>
+function Get-Frontend{
 	[cmdletBinding()]
 	param(
-	[string]$Reactapp
+	[Parameter(Mandatory=$true)]
+	[string]$Projectname="myreact-app"
 	)
 # Check if Node.js is already installed
 if (Get-Command node -ErrorAction SilentlyContinue) {
@@ -22,16 +36,30 @@ if (Get-Command create-react-app -ErrorAction SilentlyContinue) {
 } else {
     Write-Host "Installing React Application now..."
     # Install create-vite@latest react-app using npm
-    npm create vite@latest $Reactapp
+    npm create vite@latest $Projectname
 
     if ($?) {
         Write-Host "create-react-app installed successfully."
     } else {
         Write-Host "create-react-app installation failed."
     }
+# Open localhost on port no 5173
+function Get-Npmrun{
+# Run npm instalation
+Invoke-Expression "& npm install"
 }
-cd "Reactapp"
-npm install
-npm run dev
-start http://www.localhost:5173
+function Get-Devrun{
+# npm run dev
+Invoke-Expression "npm run dev &"
+}
+function Get-Reactstart{
+	start microsoft-edge:http://localhost:5173
+}
+if ( -not ([string]::IsNullorEmpty($Projectname))){
+Set-Location $Projectname && Get-Npmrun && Get-Devrun && Get-Reactstart
+}
+else{
+	Write-Host "You have to run react Application manually"
+}
+}
 }
