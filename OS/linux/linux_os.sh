ce55185b8989f650c23f3757c845703f31bd23b8
Dawalhasa <dawalhasa@gmail.com>
2025-06-04 18:16:15 +0530

    Modified operating system and linux distro code

--- OS/linux/linux_os.sh
+++ OS/linux/linux_os.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-OS=$(uname -s)
 Distro=$(uname -n)
-echo 'Provite your vite-Project Name:'
-
 
 read name
 
 # install react on linux distro
-if [[ "$OS" == "Linux" && "$Distro" == "Ubuntu" ]]; then
+if [[ "$Distro" == "Ubuntu" ]]; then
   sudo apt update
   sudo apt install -y nodejs npm
   npm create vite@latest $name
   echo "React installed via apt on Ubuntu."
-elif [[ "$OS" == "Linux" && "$Distro" == "Archi" ]]; then
+elif [[ "$Distro" == "Archi" ]]; then
   sudo pacman update
   sudo pacman install -y nodejs npm
   npm create vite@latest $name
   echo "React installed via apt on Archi Linux."
-elif [[ "$OS" == "Linux" && "$Distro" == "CentOS" ]]; then
+elif [[ "$Distro" == "CentOS" ]]; then
    sudo yum update
    sudo yum install -y nodejs npm
    npm install -g create-react-app
--- OS/os.sh
+++ OS/os.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+
+OS=$(uname -s)                                                              
+
+if [[ "$OS" == "Linux" ]]; then                    
 # install react on Linux
 source /Users/dawalhasa/bash_script/OS/linux/linux_os.sh
 
+elif [[ "$OS" == "drawin" ]]; then                  
 # insatll react on mac OS
 source /Users/dawalhasa/bash_script/OS/mac/mac_os.sh
 
+else
 # install react on window OS
 source /Users/dawalhasa/bash_script/OS/window/window_os.sh
+fi
