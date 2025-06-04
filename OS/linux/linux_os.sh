29d7044ea3329c9ba4149056fac5441ab4f66551
Dawalhasa <dawalhasa@gmail.com>
2025-06-04 18:24:14 +0530

    Modified installion of nodejs and react on linux OS more readable by add command

--- OS/linux/linux_os.sh
+++ OS/linux/linux_os.sh
@@ -6,19 +6,29 @@ read name
 
 # install react on linux distro
 if [[ "$Distro" == "Ubuntu" ]]; then
+  # update operating system using sudo
   sudo apt update
+  # install nodejs first
   sudo apt install -y nodejs npm
+  # now install react using vite@latest
   npm create vite@latest $name
   echo "React installed via apt on Ubuntu."
 elif [[ "$Distro" == "Archi" ]]; then
+  # update operating system using sudo
   sudo pacman update
+  # install nodejs first
   sudo pacman install -y nodejs npm
+  # now install react using vite@latest
   npm create vite@latest $name
+
   echo "React installed via apt on Archi Linux."
 elif [[ "$Distro" == "CentOS" ]]; then
-   sudo yum update
-   sudo yum install -y nodejs npm
-   npm install -g create-react-app
+  # update operating system using sudo
+  sudo yum update
+  # install nodejs first
+  sudo yum install -y nodejs npm
+  # now install react using vite@latest
+  npm install -g create-react-app
    echo "React installed via apt on CentOS."
 else
 	echo "Your OS don't support react installation"
